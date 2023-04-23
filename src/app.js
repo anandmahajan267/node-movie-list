4630758a770c60ffca057177158b159bc7c65004
Anand Mahajan <anandmahajan267@gmail.com>
2023-04-23 09:46:00 +0530

    Add cors package

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors');
+
 const appRouter = require('./routes/app.router');
 const app = express()
 
 // this line is required to parse the request body.
 app.use(express.json())
+app.use(cors());
 app.use('/api', appRouter);
 // configure the server port.
 app.listen(3000, () => {
