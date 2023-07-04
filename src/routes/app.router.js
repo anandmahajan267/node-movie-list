087098db18bbdf3672096411c4a6183bf5c525a3
Anand Mahajan <anandmahajan267@gmail.com>
2023-07-04 14:44:25 +0530

    router clean up

--- src/routes/app.router.js
+++ src/routes/app.router.js
@@ -10,10 +10,5 @@ router.put('/movie/:id', movieController.updateMovie);
 router.delete('/movie/:id', movieController.daleteMovie);
 router.get('/movie/:id', movieController.getMovie);
 
-// router.post('/', UserController.createUser);
-
-// router.delete('/me', auth, UserController.deleteUser);
-
-// router.get('/:id', auth, UserController.getUserById);
 
 module.exports = router;
